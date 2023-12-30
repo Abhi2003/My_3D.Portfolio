566db668ea9f4026ccfc278b5082ebf0d25ef8e3
Dungar <dungarmn2003@gmail.com>
2023-12-30 15:11:29 +0530

    fourth commit

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -18,6 +18,9 @@ const FeedbackCard = ({ index, testimonial, name, designation, company, image })
           <p>
             <span>@</span> {name}
           </p>
+          <p>
+            {designation} of {company}
+          </p>
         </div>
 
       </div>
@@ -29,16 +32,16 @@ const Feedbacks = () => {
     <div className="mt-12 bg-black-100 rounded-[20px]">
       <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
         <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>What others say</p>
-        <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+          <p className={styles.sectionSubText}>What others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
       </div>
       <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
         {testimonials.map((testimonial, index) => (
           <FeedbackCard
-          key={testimonial.name}
-          index={index}
-          {...testimonial}
+            key={testimonial.name}
+            index={index}
+            {...testimonial}
           />
         ))}
       </div>
--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import {Suspense} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+
+import CanvasLoader from '../Loader';
 
 const Earth = () => {
   return (
     <div>Earth</div>
+  )
+}
+
+const EarthCanvas = () => {
+  return(
+    <Canvas
+    shadows
+    frameloop='demand'
+    // gl={ }
+    >
+      
+    </Canvas>
   )
 }
 
